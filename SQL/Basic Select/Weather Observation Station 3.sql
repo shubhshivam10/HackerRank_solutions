683dec9412ac929c03b10752332b2b0fa728682a
shubhshivam10 <50029378+shubhshivam10@users.noreply.github.com>
2019-07-24 01:59:09 +0530

    Update Weather Observation Station 3.sql

--- SQL/Basic Select/Weather Observation Station 3.sql
+++ SQL/Basic Select/Weather Observation Station 3.sql
@@ -1,5 +1,8 @@
 -- Author: Rodney Shaghoulian
 -- Github: github.com/RodneyShag
 
-SELECT DISTINCT CITY FROM STATION
-WHERE ID % 2 = 0;
+/*SELECT DISTINCT CITY FROM STATION
+WHERE ID % 2 = 0;*/
+SELECT DISTINCT CITY 
+FROM STATION
+WHERE MOD(ID,2) = 0 ;
